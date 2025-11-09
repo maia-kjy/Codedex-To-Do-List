395280b597c32a1108583d9a369252e745436575
Ara <kamalinmasturazaini@gmail.com>
2025-11-09 21:09:09 +0800

    script.js

--- script.js
+++ script.js
@@ -32,6 +32,41 @@ public class CodedexToDoList {
             
             checkbox.addEventListener("click", function () {
                 li.classList.toggle("completed", checkbox.checked);
-});
-    }
+            });
+
+        editBtn.addEventListener("click", function () {
+          const update = prompt("Edit task:", taskSpan.textContent);
+          if (update !== null) {
+              taskSpan.textContent = update;
+              li.classList.remove("completed");
+          }
+        });
+        const completedCounter = document.getElementById("completed-counter");
+        const uncompletedCounter = document.getElementById("uncompleted-counter");
+
+        function updateCounters() {
+            const completedTasks = document.querySelectorAll(".completed").length;
+            const uncompletedTasks =
+            document.querySelectorAll("li:not(.completed)").length;
+
+            completedCounter.textContent = completedTasks;
+            uncompletedCounter.textContent = uncompletedTasks;
+            
+        }
+        checkbox.addEventListener("click", function () {
+          li.classList.toggle("completed", checkbox.checked);
+          //add the function below
+          updateCounters();
+            });
+        
+            editBtn.addEventListener("click", function () {
+            const update = prompt("Edit task:", taskSpan.textContent);
+            if (update !== null) {
+            taskSpan.textContent = update;
+            li.classList.remove("completed");
+            //add the code below
+            checkbox.checked = false;
+            updateCounters();}
+                });
+        }
 }
