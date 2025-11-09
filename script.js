3ca2060c2521585b74a1b308b283c9f4de0a3b93
Ara <kamalinmasturazaini@gmail.com>
2025-11-09 20:39:30 +0800

    script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class CodedexToDoList {
+    public static void main(String[] args) {
+
+    const inputBox = document.getElementById("input-box");
+    const listContainer = document.getElementById("list-container");
+
+    }
+}
