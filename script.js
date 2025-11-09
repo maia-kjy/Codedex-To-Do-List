72504a43970e0d23b7145b394b39ed5d9e3c1de4
Ara <kamalinmasturazaini@gmail.com>
2025-11-09 20:46:31 +0800

    script.js

--- script.js
+++ script.js
@@ -7,23 +7,23 @@ public class CodedexToDoList {
     const listContainer = document.getElementById("list-container");
 
         function addTask() {
-  const task = inputBox.value.trim();
-  if (!task) {
-    alert("Please write down a task");
-    return;
-  }
+          const task = inputBox.value.trim();
+            if (!task) {
+               alert("Please write down a task");
+            return;
+        }
+
+  li.innerHTML = `
+    <label>
+      <input type="checkbox">
+      <span>${task}</span>
+    </label>
+    <span class="edit-btn">Edit</span>
+    <span class="delete-btn">Delete</span>
+    `;
+
+  listContainer.appendChild(li);
+  inputBox.value = "";
 }
-        const li = document.createElement("li");
-
-            li.innerHTML = `
-              <label>
-            <input type="checkbox">
-            <span>${task}</span>
-              </label>
-                  <span class="edit-btn">Edit</span>
-                  <span class="delete-btn">Delete</span>
-            `;
-
-        listContainer.appendChild(li);
     }
 }
